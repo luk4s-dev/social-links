0f7b96db6211c1b3a4652ad906e4bfbc25e7bd8f
Lucas Guimarães Santana <lucks.guimaraes2@gmail.com>
2024-06-25 23:28:48 -0300

    first version conclued

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ function toggleMode() {
 
     const img = html.querySelector('#profile img')
     if(html.classList.contains('light')) {
-        img.setAttribute('src', './assets/avatar-light.png');
+        img.setAttribute('src', './assets/gs-avatar.jpg');
     } else {
-        img.setAttribute('src', './assets/avatar.png');
+        img.setAttribute('src', './assets/gs-avatar.jpg');
     }
 }
